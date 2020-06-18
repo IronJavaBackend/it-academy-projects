c8f68ecdd663a2dae222ad5babe99fd82df6ea9f
Elena Piaggio <elenapiaggio@gmail.com>
2020-06-18 13:32:06 +0200

    ADD initial project code from url of IT Academy

---
+++ src/jobs/com/jobs/domain/IPaymentRate.java
@@ -0,0 +1,5 @@
+package jobs.com.jobs.domain;
+
+public interface IPaymentRate {
+	public double pay(double salaryPerMonth);
+}
---
+++ src/jobs/com/jobs/persistence/EmployeeRepository.java
@@ -0,0 +1,28 @@
+package jobs.com.jobs.persistence;
+
+import jobs.com.jobs.domain.AbsStaffMember;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class EmployeeRepository {
+
+	private static List<AbsStaffMember> members=new ArrayList<>();
+	
+	public EmployeeRepository(){
+		
+	}
+	
+	public List<AbsStaffMember> getAllMembers(){
+		return new ArrayList<>(members);
+	}
+	
+	
+	public void addMember(AbsStaffMember member) throws Exception{
+		if(member==null) throw new Exception();
+		members.add(member);
+	}
+	
+	
+}
